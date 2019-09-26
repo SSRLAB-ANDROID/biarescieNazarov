126a053b8cc436c6d51a959a5255fd824584a181
Alexander <neverpret@gmail.com>
2019-09-26 15:13:13 +0300

    should fix changing items in list

--- app/src/main/java/by/ssrlab/tibo2019/ui/recycler/viewholders/SectionViewHolder.kt
+++ app/src/main/java/by/ssrlab/tibo2019/ui/recycler/viewholders/SectionViewHolder.kt
@@ -1,21 +1,19 @@
 package by.krokam.biarescie.ui.recycler.viewholders
 
-import android.view.View
 import android.view.ViewGroup
 import by.krokam.biarescie.R
 import by.krokam.biarescie.data.items.Section
+import by.krokam.biarescie.util.isGone
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_section.view.*
 
-class SectionViewHolder(parent : ViewGroup) : BaseViewHolder<Section>(parent, R.layout.item_section){
+class SectionViewHolder(parent: ViewGroup) : BaseViewHolder<Section>(parent, R.layout.item_section) {
 
     override fun bind(item: Section) {
         super.bind(item)
-        itemView.run {
-            if(item.namePrefix.length!=0)
-            tvTitle.text=item.namePrefix
-            else
-                tvTitle.visibility= View.GONE
+        itemView.apply {
+            tvTitle.isGone(item.namePrefix.isEmpty())
+            tvTitle.text = item.namePrefix
             tvName.text = item.name
             Glide.with(context).load(item.logo).into(photo)
         }
